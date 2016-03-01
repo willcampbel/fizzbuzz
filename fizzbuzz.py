0d49f4d3b0458382e083734c0908a00be7cea04c
willcampbel <williamcampbell@hanovernorwichschools.org>
2016-03-01 14:27:44 -0500

    Updated from Brython Server: 3/1/2016 2:27:46 PM

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,7 +1,7 @@
 """
 fizzbuzz.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Will
+Credit: Eli
 
 Assignment:
 
@@ -45,9 +45,15 @@ Fizz
 Buzz
 """
 import math 
-numbers = int(input("How many numbers shall we print?")
-fizz = int(input("For multiples of what number shall we print 'Fizz'?")
-buzz = int(input("For multiples of what number shall we print 'Buzz'?")
+numbers = int(input("How many numbers shall we print?"))
+fizz = int(input("For multiples of what number shall we print 'Fizz'?"))
+buzz = int(input("For multiples of what number shall we print 'Buzz'?"))
+for x in range(numbers+1):
+    print(x)
+    if x%fizz == 0:
+        print("Fizz")
+    if x%buzz == 0:
+        print("Buzz")
 
 
 
